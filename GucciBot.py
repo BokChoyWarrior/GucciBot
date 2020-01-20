c5bbc08cd1f03df536150bc8085c1ec16b98ba52
Harvey Cambridge <harvey.cambridge@gmail.com>
2020-01-20 12:37:26 +0000

    Logging implemented and rename of main class file

--- GucciBot.py
+++ GucciBot.py
@@ -1,8 +1,15 @@
 import traceback
 from datetime import datetime
-import discord
 from discord.ext import commands
 import json
+import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='GucciBot.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 def getConfig(path):
